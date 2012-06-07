183663c6685a2d72792852e8d58396d58957d3f4
gbalabasquer@gmail.com <gbalabasquer@gmail.com@f7158288-3251-6d70-7d7c-d049098a16fc>
2012-06-07 15:05:33 +0000

    1) Mejoras en la función de login y separación en algunas funciones 2) Agregado el registro pero aún no funciona (rompe la aplicación)

--- trunk/samples/AndroidLoginTest/src/library/LoginTask.java
+++ trunk/samples/AndroidLoginTest/src/library/LoginTask.java
@@ -1,20 +1,9 @@
 package library;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,8 +14,6 @@ import test.test3.LoginActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.util.Log;
-import android.widget.TextView;
 
 public class LoginTask extends AsyncTask<String, Void, Integer> {
 
@@ -45,8 +32,6 @@ public class LoginTask extends AsyncTask<String, Void, Integer> {
     
 	private ProgressDialog progressDialog;
 	private LoginActivity loginActivity;
-	private int id = -1;
-	private InputStream resultContent = null;
 	
 	public LoginTask(LoginActivity loginActivity, ProgressDialog progressDialog)
 	{
@@ -67,23 +52,8 @@ public class LoginTask extends AsyncTask<String, Void, Integer> {
         params.add(new BasicNameValuePair("email", arg0[0]));
         params.add(new BasicNameValuePair("password", arg0[1]));
 
-		try {
-            // defaultHttpClient
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost(loginURL);
-            httpPost.setEntity(new UrlEncodedFormEntity(params));
- 
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-            HttpEntity httpEntity = httpResponse.getEntity();
-            resultContent = httpEntity.getContent();
- 
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }		
+        CommonFunctions commonFunctions = new CommonFunctions();       
+        jObj = commonFunctions.parseJSON(commonFunctions.httpPost(loginURL, params));
 		
 		return 1;
 	}
@@ -92,30 +62,7 @@ public class LoginTask extends AsyncTask<String, Void, Integer> {
 	protected void onPostExecute(Integer headerCode)
 	{
 		progressDialog.dismiss();
-		
-		try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-            		resultContent, "iso-8859-1"), 8);
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "n");
-            }
-            resultContent.close();
-            json = sb.toString();
-            Log.e("JSON", json);
-        } catch (Exception e) {
-            Log.e("Buffer Error", "Error converting result " + e.toString());
-        }
- 
-        // try parse the string to a JSON object
-        try {
-            jObj = new JSONObject(json);
-        } catch (JSONException e) {
-            Log.e("JSON Parser", "Error parsing data " + e.toString());
-        }
-		
-		// check for login response
+
         try {
             if (jObj.getString(KEY_SUCCESS) != null) {
             	loginActivity.loginErrorMsg.setText("");
@@ -127,7 +74,8 @@ public class LoginTask extends AsyncTask<String, Void, Integer> {
                     JSONObject json_user = jObj.getJSONObject("user");
 
                     // Clear all previous data in database
-                    //userFunction.logoutUser(getApplicationContext());
+                    UserFunctions userFunction = new UserFunctions();
+                    userFunction.logoutUser(loginActivity.getApplicationContext());
                     db.addUser(json_user.getString(KEY_NAME), json_user.getString(KEY_EMAIL), jObj.getString(KEY_UID), json_user.getString(KEY_CREATED_AT));                       
 
                     // Launch Dashboard Screen
