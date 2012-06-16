38fa0517d5d2b708b657ecf329c04c2377401a13
abdalaalejo@gmail.com <abdalaalejo@gmail.com@f7158288-3251-6d70-7d7c-d049098a16fc>
2012-06-16 04:00:34 +0000

    Renombrando TrMobil.. a LoginActivity. Arreglando la navegabilidad para que se pueda entrar por el login y pasar a los tabs.

---
+++ trunk/tr-mobile-app/src/com/menatwork/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.menatwork;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+
+import com.mentatwork.R;
+
+public class LoginActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+
+		setupLoginWithLinkedinButton();
+	}
+
+	private void setupLoginWithLinkedinButton() {
+		final ImageButton button = (ImageButton) findViewById(R.id.login_button_linkedin);
+		button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(LoginActivity.this,
+						MainActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+}
