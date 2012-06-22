bec41b0827bc84dace65da46f1502763c50b5f50
abdalaalejo@gmail.com <abdalaalejo@gmail.com@f7158288-3251-6d70-7d7c-d049098a16fc>
2012-06-22 21:06:47 +0000

    - Implementing user interface for normal registration use case. - Adding basic (unvalidated) navigation between screens. - Refactoring strings.xml

---
+++ trunk/tr-mobile-app/src/com/menatwork/register/RegisterSkillsActivity.java
@@ -0,0 +1,31 @@
+package com.menatwork.register;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.menatwork.LoginActivity;
+import com.menatwork.MainActivity;
+import com.menatwork.utils.StartActivityOnClickListener;
+import com.mentatwork.R;
+
+public class RegisterSkillsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register_skills);
+		setupButtons();
+	}
+
+	private void setupButtons() {
+		Button finishButton = (Button) findViewById(R.id.register_skills_button_finish);
+		Button cancelButton = (Button) findViewById(R.id.register_skills_button_cancel);
+
+		finishButton.setOnClickListener(new StartActivityOnClickListener(this,
+				MainActivity.class));
+		cancelButton.setOnClickListener(new StartActivityOnClickListener(this,
+				LoginActivity.class));
+	}
+}
