6a171262b22f9a56a7d957457cd9686a211f9a87
abdalaalejo@gmail.com <abdalaalejo@gmail.com@f7158288-3251-6d70-7d7c-d049098a16fc>
2012-06-23 19:40:11 +0000

    Orchestrating activities to pass along data among each other.

--- trunk/tr-mobile-app/src/com/menatwork/register/PasswordActivity.java
+++ trunk/tr-mobile-app/src/com/menatwork/register/PasswordActivity.java
@@ -1,14 +1,13 @@
 package com.menatwork.register;
 
-import com.menatwork.LoginActivity;
-import com.menatwork.utils.StartActivityOnClickListener;
-import com.mentatwork.R;
-
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class PasswordActivity extends Activity {
+import com.menatwork.LoginActivity;
+import com.mentatwork.R;
+
+public class PasswordActivity extends DataInputActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,9 +20,20 @@ public class PasswordActivity extends Activity {
 		Button nextButton = (Button) findViewById(R.id.register_password_button_next);
 		Button cancelButton = (Button) findViewById(R.id.register_password_button_cancel);
 
-		nextButton.setOnClickListener(new StartActivityOnClickListener(this,
-				SkillsActivity.class));
-		cancelButton.setOnClickListener(new StartActivityOnClickListener(this,
-				LoginActivity.class));
+		nextButton.setOnClickListener(new StartActivityPassingDataListener(
+				this, SkillsActivity.class));
+		cancelButton.setOnClickListener(new StartActivityPassingDataListener(
+				this, LoginActivity.class));
+	}
+
+	Bundle getConfiguredData() {
+		Bundle data = new Bundle();
+		data.putString(RegistrationExtras.PASSWORD, this.getControlPassword()
+				.getText().toString());
+		return data;
+	}
+
+	private TextView getControlPassword() {
+		return (TextView) findViewById(R.id.register_password_password1);
 	}
 }
