d4405e982a9eb6365092009c660f17a270f98928
abdalaalejo@gmail.com <abdalaalejo@gmail.com@f7158288-3251-6d70-7d7c-d049098a16fc>
2012-06-23 18:24:13 +0000

    Adding password page, renaming activities (redundant with package).
    STARTED - task 1: Implementar registracion de usuarios 
    http://code.google.com/p/talent-radar/issues/detail?id=1

---
+++ trunk/tr-mobile-app/src/com/menatwork/register/RegistrationExtras.java
@@ -0,0 +1,7 @@
+package com.menatwork.register;
+
+class RegistrationExtras {
+	static final String EMAIL = "email";
+	static final String NICKNAME = "nickname";
+	static final String REALNAME = "realname";
+}
--- trunk/tr-mobile-app/src/com/menatwork/register/SkillsActivity.java
+++ trunk/tr-mobile-app/src/com/menatwork/register/SkillsActivity.java
@@ -2,6 +2,7 @@ package com.menatwork.register;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import com.menatwork.LoginActivity;
@@ -9,19 +10,31 @@ import com.menatwork.MainActivity;
 import com.menatwork.utils.StartActivityOnClickListener;
 import com.mentatwork.R;
 
-public class RegisterSkillsActivity extends Activity {
+public class SkillsActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.register_skills);
 		setupButtons();
+
+		try {
+			Bundle extras = getIntent().getExtras();
+			Log.d("alme", RegistrationExtras.REALNAME);
+			String realname = extras.getString(RegistrationExtras.REALNAME);
+			String nickname = extras.getString(RegistrationExtras.NICKNAME);
+			String email = extras.getString(RegistrationExtras.EMAIL);
+			Log.d("alme", "realname " + realname + " nickname " + nickname
+					+ " email " + email);
+		} catch (NullPointerException e) {
+			Log.e("alme", "screwed up dude", e);
+		}
 	}
 
 	private void setupButtons() {
 		Button finishButton = (Button) findViewById(R.id.register_skills_button_finish);
 		Button cancelButton = (Button) findViewById(R.id.register_skills_button_cancel);
-
+		
 		finishButton.setOnClickListener(new StartActivityOnClickListener(this,
 				MainActivity.class));
 		cancelButton.setOnClickListener(new StartActivityOnClickListener(this,
