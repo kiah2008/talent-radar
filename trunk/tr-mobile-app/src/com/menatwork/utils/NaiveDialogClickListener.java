d629c5ecacb53dbbdf305e8d3a130b5f646d03a1
abdalaalejo@gmail.com <abdalaalejo@gmail.com@f7158288-3251-6d70-7d7c-d049098a16fc>
2012-06-28 23:04:38 +0000

    Login cooked (TODO review login responses). Refactoring GonzaUtils to be more useful/loggable.

---
+++ trunk/tr-mobile-app/src/com/menatwork/utils/NaiveDialogClickListener.java
@@ -0,0 +1,13 @@
+package com.menatwork.utils;
+
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+
+public class NaiveDialogClickListener implements OnClickListener {
+
+	@Override
+	public void onClick(DialogInterface arg0, int arg1) {
+		// do nothing
+	}
+
+}
