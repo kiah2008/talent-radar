1d5f1be8b53f79a8f139ba9f6b2bbb9301d248ab
gbalabasquer <gbalabasquer@gmail.com>
2012-07-14 18:11:45 +0000

    Share Location and Get Users in one action

--- tr-service/app/Controller/UsersOnlineController.php
+++ tr-service/app/Controller/UsersOnlineController.php
@@ -8,10 +8,42 @@ class UsersOnlineController extends AppController {
 	public function beforeFilter() {
 		parent::beforeFilter();
 		
-		$this->Auth->allow('app_shareLocation', 'app_getUsers');
+		$this->Auth->allow('app_getUsers', 'app_shareLocationAndGetUsers');
 	}
 	
-	public function app_shareLocation() {
+	public function _app_getUsers($data) {
+		$conditions['sqrt(pow(69.1 * (UsersOnline.latitude - '.$data['UsersOnline']['latitude'].'), 2) + pow(53.0 * (UsersOnline.longitude - '.$data['UsersOnline']['latitude'].'), 2)) <'] = (1/1.609344); //Calculate if distance is lower than input. (Convertion miles to kilometres)
+		$conditions['UsersOnline.user_id <>'] = $data['UsersOnline']['user_id'];
+		$conditions['TIMEDIFF(ADDTIME(UsersOnline.modified, UsersOnline.duration), NOW()) >'] = 0;
+					
+		return $this->UsersOnline->find('all', array('conditions' => $conditions));
+	}
+	
+	public function app_getUsers() {
+		if(!empty($this->data)) {
+			$response['status'] = 'ok';
+			$response['result']['status'] = 'error';
+			
+			$this->UsersOnline->set($this->data);	
+			if($ok = ($invalidFields = $this->UsersOnline->invalidFields()) ? false : true)
+			{
+				$response['result']['users'] = $this->_app_getUsers($this->data);
+				$response['result']['status'] = 'ok';
+			}
+			
+			if(!$ok) {
+				$response['result']['invalidFields'] = $invalidFields;
+			}
+			
+			$this->set('response', $response);
+		}
+		else {
+			$this->loadModel('User');
+			$this->set('users', $this->User->find('list', array('fields' => array('id', 'id'))));
+		}
+	}
+	
+	public function app_shareLocationAndGetUsers() {
 		if(!empty($this->data)) {
 			$response['status'] = 'ok';
 			$response['result']['status'] = 'error';
@@ -23,36 +55,10 @@ class UsersOnlineController extends AppController {
 					$this->request->data['UsersOnline']['id'] = $userAlreadyOnline['UsersOnline']['id'];
 				}
 				
-				if($ok = $this->UsersOnline->save($this->data)) {
+				if($ok = $this->UsersOnline->save($this->data)) {					
+					$response['result']['users'] = $this->_app_getUsers($this->data);
 					$response['result']['status'] = 'ok';
 				}
-			}
-
-			if(!$ok) {
-				$response['result']['invalidFields'] = $invalidFields;
-			}
-
-			$this->set('response', $response);
-		}
-		else {
-			$this->loadModel('User');
-			$this->set('users', $this->User->find('list', array('fields' => array('id', 'id'))));
-		}
-	}
-	
-	public function app_getUsers() {
-		if(!empty($this->data)) {
-			$response['status'] = 'ok';
-			$response['result']['status'] = 'error';
-			
-			$this->UsersOnline->set($this->data);	
-			if($ok = ($invalidFields = $this->UsersOnline->invalidFields()) ? false : true)
-			{
-				$conditions['sqrt(pow(69.1 * (UsersOnline.latitude - '.$this->data['UsersOnline']['latitude'].'), 2) + pow(53.0 * (UsersOnline.longitude - '.$this->data['UsersOnline']['latitude'].'), 2)) <'] = (1/1.609344); //Calculate if distance is lower than input. (Convertion miles to kilometres)
-				$conditions['UsersOnline.user_id <>'] = $this->data['UsersOnline']['user_id'];
-				$conditions['TIMEDIFF(ADDTIME(UsersOnline.modified, UsersOnline.duration), NOW()) >'] = 0;
-				$response['result']['users'] = $this->UsersOnline->find('all', array('conditions' => $conditions));
-				$response['result']['status'] = 'ok';
 			}
 			
 			if(!$ok) {
