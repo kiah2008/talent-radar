d259a4e7384409c7bf8928e6e62dd89b860a9d4a
oliva.miguelh@gmail.com <oliva.miguelh@gmail.com@f7158288-3251-6d70-7d7c-d049098a16fc>
2012-07-17 22:25:54 +0000

    Radar view now available (first shot)

--- trunk/tr-mobile-app/src/com/menatwork/MainActivity.java
+++ trunk/tr-mobile-app/src/com/menatwork/MainActivity.java
@@ -13,35 +13,29 @@ public class MainActivity extends TabActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		// final Resources res = getResources(); // Resource object to get
+		final Resources res = getResources();
+
 		// Drawables
 		final TabHost tabHost = getTabHost(); // The activity TabHost
 		TabHost.TabSpec spec; // Resusable TabSpec for each tab
-		Intent intent; // Reusable Intent for each tab
 
-		// Create an Intent to launch an Activity for the tab (to be reused)
-		intent = new Intent().setClass(this, DashboardActivity.class);
-		Resources res = getResources();
-		// Initialize a TabSpec for each tab and add it to the TabHost
-		spec = tabHost.newTabSpec("dashboard").setIndicator("Dashboard" //
-				, res.getDrawable(R.drawable.ic_tab_dashboard_unselected) //
-				).setContent(intent);
+		final Intent dashboardIntent = new Intent(this, DashboardActivity.class);
+		spec = tabHost.newTabSpec("dashboard");
+		spec.setIndicator("Dashboard", res.getDrawable(R.drawable.icon_dashboard_tab));
+		spec.setContent(dashboardIntent);
 		tabHost.addTab(spec);
 
-		// Do the same for the other tabs
-		intent = new Intent().setClass(this, RadarActivity.class);
-		spec = tabHost.newTabSpec("radar").setIndicator("Radar" //
-				, res.getDrawable(R.drawable.ic_tab_radar_unselected) //
-				).setContent(intent);
+		final Intent radarIntent = new Intent(this, RadarActivity.class);
+		spec = tabHost.newTabSpec("radar").setIndicator("Radar", res.getDrawable(R.drawable.icon_radar_tab))
+				.setContent(radarIntent);
 		tabHost.addTab(spec);
 
-		intent = new Intent().setClass(this, ProfileActivity.class);
+		final Intent profileIntent = new Intent(this, ProfileActivity.class);
 		spec = tabHost.newTabSpec("profile").setIndicator("Profile" //
-				, res.getDrawable(R.drawable.ic_tab_profile_unselected) //
-				).setContent(intent);
+				, res.getDrawable(R.drawable.icon_profile_tab) //
+				).setContent(profileIntent);
 		tabHost.addTab(spec);
 
-		tabHost.setCurrentTab(0);
 	}
 
 }
