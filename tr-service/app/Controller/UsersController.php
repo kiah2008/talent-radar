44f88336d853c3f7d0593b1ee5aa214917962441
gbalabasquer <gbalabasquer@gmail.com>
2012-07-18 00:27:07 +0000

    Fix

--- tr-service/app/Controller/UsersController.php
+++ tr-service/app/Controller/UsersController.php
@@ -19,7 +19,7 @@ class UsersController extends AppController {
 			$response['status'] = 'ok';
 			$response['result']['status'] = 'error';
 			
-			if($response['result']['user'] = $this->User->find('first', array('conditions' => array('User.id' => $this->data['User']['id'])))) {
+			if($response['result']['user'] = $this->User->find('first', array('User.id' => $this->data['User']['id']))) {
 				$response['result']['status'] = 'ok';
 			}
 			
@@ -58,8 +58,7 @@ class UsersController extends AppController {
 			$response['status'] = 'ok';
 			$response['result']['status'] = 'error';
 			
-			if($user = $this->User->find('first', array('conditions' => array('User.email' => $this->data['User']['email'], 'User.password' => $this->Auth->password($this->data['User']['password']))))) {
-				$response['result']['User'] = $user['User'];
+			if($response['result']['user'] = $this->User->find('first', array('conditions' => array('User.email' => $this->data['User']['email'], 'User.password' => $this->Auth->password($this->data['User']['password']))))) {
 				$response['result']['status'] = 'ok';
 			}
 			
