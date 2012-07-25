b1058ee95cb90b87641f799d6855d8c008143ad8
oliva.miguelh@gmail.com <oliva.miguelh@gmail.com@f7158288-3251-6d70-7d7c-d049098a16fc>
2012-07-25 20:00:59 +0000

    Some minor changes to interact with the service.

--- trunk/tr-mobile-app/src/com/menatwork/radar/RadarActivity.java
+++ trunk/tr-mobile-app/src/com/menatwork/radar/RadarActivity.java
@@ -1,9 +1,16 @@
 package com.menatwork.radar;
 
+import android.os.Bundle;
+
 import com.menatwork.R;
 import com.menatwork.TalentRadarActivity;
 
 public class RadarActivity extends TalentRadarActivity {
+
+	@Override
+	protected int getViewLayoutId() {
+		return R.layout.radar;
+	}
 
 	@Override
 	protected void setupButtons() {
@@ -16,8 +23,8 @@ public class RadarActivity extends TalentRadarActivity {
 	}
 
 	@Override
-	protected int getViewLayoutId() {
-		return R.layout.radar;
+	protected void postCreate(final Bundle savedInstanceState) {
+		// bindService(new Intent(this, RadarService.class), conn, flags);
 	}
 
 }
--- trunk/tr-mobile-app/src/com/menatwork/radar/RadarService.java
+++ trunk/tr-mobile-app/src/com/menatwork/radar/RadarService.java
@@ -3,12 +3,30 @@ package com.menatwork.radar;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.widget.Toast;
 
 public class RadarService extends Service {
 
 	@Override
+	public int onStartCommand(final Intent intent, final int flags,
+			final int startId) {
+		Toast.makeText(this, "Iniciando servicio de Radar", Toast.LENGTH_SHORT).show();
+		// TODO - dunno if it has to do something - miguel - 25/07/2012
+		return START_REDELIVER_INTENT;
+	}
+
+	@Override
 	public IBinder onBind(final Intent intent) {
-		throw new UnsupportedOperationException("Service.onBind");
+		// TODO - dunno if it has to do something - miguel - 25/07/2012
+		Toast.makeText(this, "Bindeando servicio de Radar", Toast.LENGTH_SHORT).show();
+
+		return null;
+	}
+
+	@Override
+	public void onDestroy() {
+		// TODO - Clean service state - miguel - 25/07/2012
+		Toast.makeText(this, "Finalizando servicio de Radar", Toast.LENGTH_SHORT).show();
 	}
 
 }
