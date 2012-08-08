42ddbd8192a97afeb043ac1de2683fabafce9737
oliva.miguelh@gmail.com <oliva.miguelh@gmail.com@f7158288-3251-6d70-7d7c-d049098a16fc>
2012-08-08 04:29:29 +0000

    Some comments added and TODOs deleted.

--- trunk/tr-mobile-app/src/com/menatwork/service/ResponseException.java
+++ trunk/tr-mobile-app/src/com/menatwork/service/ResponseException.java
@@ -2,29 +2,23 @@ package com.menatwork.service;
 
 public class ResponseException extends RuntimeException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public ResponseException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
-	public ResponseException(String detailMessage, Throwable throwable) {
+	public ResponseException(final String detailMessage,
+			final Throwable throwable) {
 		super(detailMessage, throwable);
-		// TODO Auto-generated constructor stub
 	}
 
-	public ResponseException(String detailMessage) {
+	public ResponseException(final String detailMessage) {
 		super(detailMessage);
-		// TODO Auto-generated constructor stub
 	}
 
-	public ResponseException(Throwable throwable) {
+	public ResponseException(final Throwable throwable) {
 		super(throwable);
-		// TODO Auto-generated constructor stub
 	}
 
 }
