9f67dd6c448edb2ea175f6cf5a497c81386efb23
oliva.miguelh@gmail.com <oliva.miguelh@gmail.com@f7158288-3251-6d70-7d7c-d049098a16fc>
2012-08-29 21:46:55 +0000

    SVN's mistake, shh.

---
+++ trunk/tr-mobile-app/src/com/menatwork/TalentRadarActivity.java
@@ -0,0 +1,54 @@
+package com.menatwork;
+
+import android.app.Activity;
+import android.app.Application;
+
+import com.menatwork.model.User;
+import com.menatwork.service.GetUser;
+import com.menatwork.service.GetUserSkills;
+import com.menatwork.service.response.GetUserResponse;
+import com.menatwork.service.response.GetUserSkillsResponse;
+
+public class TalentRadarActivity extends Activity {
+
+	/**
+	 * {@link TalentRadarApplication#isRunningOnEmulator()}
+	 */
+	public boolean isRunningOnEmulator() {
+		return getTalentRadarApplication().isRunningOnEmulator();
+	}
+
+	/**
+	 * Retrieves the {@link Application} object casted to a
+	 * {@link TalentRadarApplication}
+	 * 
+	 * @return {@link TalentRadarApplication} for the app
+	 */
+	public TalentRadarApplication getTalentRadarApplication() {
+		return (TalentRadarApplication) getApplication();
+	}
+
+	/* ****************************************** */
+	/* ********* Business' commons ************** */
+	/* ****************************************** */
+
+	protected User getLocalUser() {
+		return getTalentRadarApplication().getLocalUser();
+	}
+
+	protected User getUserById(final String userid) {
+		try {
+			final GetUser getUser = GetUser.newInstance(this, userid);
+			final GetUserResponse response = getUser.execute();
+			final GetUserSkills getUserSkills = GetUserSkills.newInstance(this, userid);
+			final User user = response.getUser();
+			final GetUserSkillsResponse userSkillsResponse = getUserSkills.execute();
+			user.setSkills(userSkillsResponse.getSkills());
+			return user;
+		} catch (final Exception e) {
+			// TODO get this right please
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
