9e91887b5273d3909990f69a6ee9d6496dcd6c66
oliva.miguelh <oliva.miguelh@gmail.com>
2012-09-14 06:23:36 +0000

    Minor changes to documentation and class names.

--- tr-mobile-app/src/com/menatwork/preferences/TalentRadarPreferencesListener.java
+++ tr-mobile-app/src/com/menatwork/preferences/TalentRadarPreferencesListener.java
@@ -2,6 +2,16 @@ package com.menatwork.preferences;
 
 public interface TalentRadarPreferencesListener {
 
+	/**
+	 * Notifies whenever there were changes made to the application's
+	 * preferences.
+	 *
+	 * @param changes
+	 *            It knows about which preferences were changed affecting which
+	 *            parts of the application
+	 * @param preferences
+	 *            Preferences which were modified, should have the new values
+	 */
 	void onPreferencesChanged(PreferencesChanges changes,
 			TalentRadarPreferences preferences);
 
