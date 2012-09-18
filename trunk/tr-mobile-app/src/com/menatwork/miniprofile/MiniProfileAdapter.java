b2ac68922e0c09db6f5ec9b0fefda6b55dda0be9
abdalaalejo@gmail.com <abdalaalejo@gmail.com@f7158288-3251-6d70-7d7c-d049098a16fc>
2012-09-18 22:28:09 +0000

    Pimping up radar-people list.

--- trunk/tr-mobile-app/src/com/menatwork/miniprofile/MiniProfileAdapter.java
+++ trunk/tr-mobile-app/src/com/menatwork/miniprofile/MiniProfileAdapter.java
@@ -1,7 +1,5 @@
 package com.menatwork.miniprofile;
 
-
-
 import android.app.Activity;
 import android.content.Intent;
 import android.view.LayoutInflater;
@@ -25,8 +23,8 @@ public class MiniProfileAdapter extends ArrayAdapter<MiniProfileItemRow> {
 	private final int layoutResourceId;
 	private final MiniProfileItemRow items[];
 
-	public MiniProfileAdapter(final Activity activity, final int layoutResourceId,
-			final MiniProfileItemRow[] items) {
+	public MiniProfileAdapter(final Activity activity,
+			final int layoutResourceId, final MiniProfileItemRow[] items) {
 		super(activity, layoutResourceId, items);
 		this.layoutResourceId = layoutResourceId;
 		this.activity = activity;
@@ -37,7 +35,8 @@ public class MiniProfileAdapter extends ArrayAdapter<MiniProfileItemRow> {
 	 * Is called everytime a row item is displayed.
 	 */
 	@Override
-	public View getView(final int position, final View convertView, final ViewGroup parent) {
+	public View getView(final int position, final View convertView,
+			final ViewGroup parent) {
 		View row = convertView;
 		MiniProfileItemRowHolder holder;
 
@@ -47,9 +46,12 @@ public class MiniProfileAdapter extends ArrayAdapter<MiniProfileItemRow> {
 
 			// initialize mini profile item row holder 'le cache'
 			holder = new MiniProfileItemRowHolder();
-			holder.txtUsername = (TextView) row.findViewById(R.id.mini_profile_username);
-			holder.txtHeadline = (TextView) row.findViewById(R.id.mini_profile_headline);
-			holder.imgPicture = (ImageView) row.findViewById(R.id.mini_profile_user_pic);
+			holder.txtUsername = (TextView) row
+					.findViewById(R.id.mini_profile_username);
+			holder.txtHeadline = (TextView) row
+					.findViewById(R.id.mini_profile_headline);
+			holder.imgPicture = (ImageView) row
+					.findViewById(R.id.mini_profile_user_pic);
 
 			// magic needed
 			row.setTag(holder);
@@ -66,7 +68,8 @@ public class MiniProfileAdapter extends ArrayAdapter<MiniProfileItemRow> {
 		return row;
 	}
 
-	private void initializeRow(final View row, final MiniProfileItemRowHolder holder,
+	private void initializeRow(final View row,
+			final MiniProfileItemRowHolder holder,
 			final MiniProfileItemRow miniProfileItem) {
 		// sets stuff
 		holder.txtUsername.setText(miniProfileItem.getUsername());
@@ -78,24 +81,31 @@ public class MiniProfileAdapter extends ArrayAdapter<MiniProfileItemRow> {
 			new LoadProfilePictureTask(activity, holder.imgPicture,
 					miniProfileItem.getPicture()).execute();
 		// find thy buttons
-		final Button seeProfileButton = (Button) row.findViewById(R.id.mini_profile_see_profile_button);
-		final Button pingButton = (Button) row.findViewById(R.id.mini_profile_ping_button);
-		final Button saveContactButton = (Button) row.findViewById(R.id.mini_profile_save_contact_button);
+		final Button seeProfileButton = (Button) row
+				.findViewById(R.id.mini_profile_see_profile_button);
+		final Button pingButton = (Button) row
+				.findViewById(R.id.mini_profile_ping_button);
+		final Button saveContactButton = (Button) row
+				.findViewById(R.id.mini_profile_save_contact_button);
 
 		// set buttons' on click listeners
 		seeProfileButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(final View v) {
-				final Intent intent = new Intent(activity, ProfileActivity.class);
-				intent.putExtra(ProfileActivity.EXTRAS_USERID, miniProfileItem.getUserId());
+				final Intent intent = new Intent(activity,
+						ProfileActivity.class);
+				intent.putExtra(ProfileActivity.EXTRAS_USERID,
+						miniProfileItem.getUserId());
 				activity.startActivity(intent);
 			}
 		});
 		pingButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(final View v) {
-				final String localUserId = getTalentRadarApplication().getLocalUser().getId();
-				new PingTask(activity).execute(localUserId, miniProfileItem.getUserId());
+				final String localUserId = getTalentRadarApplication()
+						.getLocalUser().getId();
+				new PingTask(activity).execute(localUserId,
+						miniProfileItem.getUserId());
 			}
 		});
 		saveContactButton.setOnClickListener(new OnClickListener() {
@@ -103,9 +113,10 @@ public class MiniProfileAdapter extends ArrayAdapter<MiniProfileItemRow> {
 			public void onClick(final View v) {
 				// TODO - save the contact which means... I suppose some other
 				// kind of activity, right? - boris - 17/08/2012
-				Toast.makeText(activity,
-						"saving the contact " + miniProfileItem.getUsername() + "... ohh, WTF!!!",
-						Toast.LENGTH_SHORT).show();
+				Toast.makeText(
+						activity,
+						"saving the contact " + miniProfileItem.getUsername()
+								+ "... ohh, WTF!!!", Toast.LENGTH_SHORT).show();
 			}
 		});
 	}
