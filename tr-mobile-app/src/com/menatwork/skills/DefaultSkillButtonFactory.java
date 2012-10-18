61ee3fc0cdc7f478d2c74ac47fe3a02789be0409
abdalaalejo <abdalaalejo@gmail.com>
2012-10-18 22:02:19 +0000

    Some minor fixes.

--- tr-mobile-app/src/com/menatwork/skills/DefaultSkillButtonFactory.java
+++ tr-mobile-app/src/com/menatwork/skills/DefaultSkillButtonFactory.java
@@ -1,8 +1,5 @@
 package com.menatwork.skills;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import android.content.Context;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
@@ -29,27 +26,31 @@ public class DefaultSkillButtonFactory implements SkillButtonFactory {
 		return button;
 	}
 
-	Map<Context, Button> emptyButtonsCache = new HashMap<Context, Button>();
-
 	@Override
 	public Button getEmptySkillsButton(final Context caller) {
-		final Button cachedEmptyButton = emptyButtonsCache.get(caller);
-		if (cachedEmptyButton != null)
-			return cachedEmptyButton;
-		else {
-			final Button button = createButton(caller);
-			button.setText(R.string.profile_no_skills_button);
-			button.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-					LayoutParams.WRAP_CONTENT));
-			button.setEnabled(false);
-			emptyButtonsCache.put(caller, button);
-			return button;
-		}
+		final Button button = new EmptySkillButton(caller);
+		button.setText(R.string.profile_no_skills_button);
+		button.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT));
+		button.setEnabled(false);
+		return button;
 	}
 
 	private Button createButton(final Context caller) {
 		final Button button = new Button(caller);
 		return button;
+	}
+
+	public static class EmptySkillButton extends Button {
+		public EmptySkillButton(final Context context) {
+			super(context);
+		}
+
+		@Override
+		public boolean equals(final Object o) {
+			return o instanceof EmptySkillButton;
+
+		}
 	}
 
 }
